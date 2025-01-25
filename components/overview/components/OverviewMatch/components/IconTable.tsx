d1b232550ec24d86e2369e8f632b805bf691a78b
Minh Quan Le Kim <minhquanlekim@Minhs-MacBook-Pro.local>
2025-01-25 12:39:10 +0700

    feature: build complete UI

---
+++ components/overview/components/OverviewMatch/components/IconTable.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+const IconTable: React.FC<React.SVGProps<SVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="12"
+    fill="none"
+    viewBox="0 0 20 12"
+  >
+    <path
+      fill="#8D8E92"
+      fillRule="evenodd"
+      d="M17.02 11.01V6.866h1.61v4.144a.277.277 0 0 1-.276.277h-1.058a.277.277 0 0 1-.277-.277M.275 5.255h19.447a.277.277 0 0 1 .277.277V6.59a.277.277 0 0 1-.277.276H.276A.277.277 0 0 1 0 6.59V5.532a.277.277 0 0 1 .276-.277m.685-1.461H19.04a.277.277 0 0 0 .276-.277V.99a.277.277 0 0 0-.276-.277H.96A.277.277 0 0 0 .685.99v2.527a.277.277 0 0 0 .276.277m.408 7.216V6.866H2.98v4.144a.277.277 0 0 1-.276.277H1.646a.277.277 0 0 1-.277-.277"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+);
+
+export default IconTable;
