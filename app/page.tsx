f73bdadc2e2cc6ff58c3b9e26a082b578190f40e
Minh Quan Le Kim <minhquanlekim@Minhs-MacBook-Pro.local>
2025-02-06 12:43:30 +0700

    fix: router bug

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,9 @@
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import { URL_OVERVIEW_PAGE } from '@/components/constant/common'
-
 export default function Home() {
   const router = useRouter()
-  
-  useEffect(() => {
-    router.replace(URL_OVERVIEW_PAGE)
-  }, [router])
+  router.replace(URL_OVERVIEW_PAGE)
   
   return null
 }
